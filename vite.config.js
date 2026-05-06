ad4d5e37465a74ac95a9446eb4954d54506d0fda
Ali Yousef <aliyousef.aca@gmail.com>
2026-05-06 19:57:47 +0300

    Use root asset base on Vercel

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/psrs-reference/",
+  base: process.env.VERCEL ? "/" : "/psrs-reference/",
   plugins: [react()],
 });
